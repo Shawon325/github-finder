b977c030eec0c045f42ac019270ca6e065ddd256
Bardock325 <mynameisshawon325@gmail.com>
2020-08-20 23:23:22 +0600

    project complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 
-import React, { Fragment } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import About from './components/pages/About';
 import Alert from './components/layouts/Alert';
 import AlertState from './context/alert/AlertState';
 import GithubState from './context/github/GithubState';
+import Home from './components/pages/Home';
 import Navbar from './components/layouts/Navbar';
-import Search from './components/users/Search';
+import NotFound from './components/pages/NotFound';
+import React from 'react';
 import User from './components/users/User';
-import Users from './components/users/Users';
 
 const App = () => {
     return (
@@ -22,22 +22,14 @@ const App = () => {
                         <div className="container">
                             <Alert />
                             <Switch>
-                                <Route
-                                    exact
-                                    path="/"
-                                    render={props => (
-                                        <Fragment>
-                                            <Search />
-                                            <Users />
-                                        </Fragment>
-                                    )}
-                                />
+                                <Route exact path="/" component={Home} />
                                 <Route exact path="/about" component={About} />
                                 <Route
                                     exact
                                     path="/user/:login"
                                     component={User}
                                 />
+                                <Route component={NotFound} />
                             </Switch>
                         </div>
                     </div>
